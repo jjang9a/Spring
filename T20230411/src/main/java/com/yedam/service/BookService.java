5d96aaaeb94b5dcbcce179ff928fd3b2fc04e601
jjang9a <123717118+jjang9a@users.noreply.github.com>
2023-04-12 10:20:16 +0900

    Delete T20230411 directory

--- T20230411/src/main/java/com/yedam/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.yedam.service;
-
-import com.yedam.domain.BookVO;
-
-public interface BookService {
-
-	public int getNext(); // 다음 도서번호
-	public void registerBook(BookVO vo); // 도서 등록
-}
